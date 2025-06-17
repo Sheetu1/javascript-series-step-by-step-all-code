43b71c4f154573e55adbb65e9d9d0591e083d68d
Sheetu1 <sheetusharma548@gmail.com>
2025-06-17 17:47:35 +0800

    Objects

--- how js run/script.js
+++ how js run/script.js
@@ -1,3 +1,9 @@
-let Username = 'sheetal'
+
+debugger
+console.log( Username);
+
+var Username = 'sheetal'
 let Lastname = 'sharma'
+
 let userInfo = "Hii my name is" + " " + Username + " " + Lastname;
+
---
+++ objects/script.js
@@ -0,0 +1,14 @@
+let user = {
+    FirstName:"sheetal",
+    LastName:"sharma",
+    extraInfo:{
+        address:"khair",  
+        city:"Aligarh"
+    }
+}
+console.log(user.extraInfo.city);
+// user.age = 20
+// delete user.FirstName
+console.log("FirstName" in user);
+
+
