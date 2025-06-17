9a6dae26f02464ffaf82d33929dc2604c6f80f94
Sheetu1 <sheetusharma548@gmail.com>
2025-06-17 19:17:21 +0800

    All methods

--- Arrays/script.js
+++ Arrays/script.js
@@ -13,16 +13,38 @@
 // console.log(FruitsName);
 
 let colors = ['red','blue','green','pink','purple']
+let Animals = ['Dog','rat','mouse']
+let arr = [1,2,3,4,5]
 console.log(colors);
-colors.push('green','orange')
+// colors.push('green','orange')
+// console.log(colors);
+// colors.pop()
+// console.log(colors);
+// colors.unshift('ranbow','black','white')
+// console.log(colors);
+// colors.shift()
+// console.log(colors);
+// concat original aray ko modify nhi krta h 
+// colors.concat(Animals)
 console.log(colors);
-colors.pop()
+// console.log(colors.indexOf('red'));
+// colors.includes('pink')
+// console.log(colors);
+// colors.reverse()
+// console.log(colors);
+// colors.sort()
+// console.log(colors);
+colors.slice(3,4)
 console.log(colors);
-colors.unshift('ranbow','black','white')
+colors.splice(2,1,'gray')
 console.log(colors);
 
-colors.shift()
-console.log(colors);
+
+
+
+
+
+
 
 
 
