867acf3058c6844e82605a12ea26dadb953d8741
Sheetu1 <sheetusharma548@gmail.com>
2025-06-17 18:41:35 +0800

    Array methods

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,1 +1,29 @@
-console.log('hello');
+// strings  we can't change the original value bcz string is a primitive type and immutable 
+// let str = "Sheetal"
+// console.log(str[4]);
+
+// Arrays we can change  the value 
+// Array methods
+// push,pop,shift,unshift,slice,splice,concat,indexOf,includes,reverse,sort
+// let FruitsName = ['Apple','Mango','Grapes','Banana']
+// console.log(FruitsName);   
+// FruitsName[2] = 'Orange'
+// console.log(FruitsName);
+// FruitsName[4] = "Dates"
+// console.log(FruitsName);
+
+let colors = ['red','blue','green','pink','purple']
+console.log(colors);
+colors.push('green','orange')
+console.log(colors);
+colors.pop()
+console.log(colors);
+colors.unshift('ranbow','black','white')
+console.log(colors);
+
+colors.shift()
+console.log(colors);
+
+
+
+
